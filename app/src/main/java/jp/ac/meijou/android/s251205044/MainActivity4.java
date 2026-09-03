a32dcc08a83a5dae69c5b8ab0b858c1c564bc843
Takumi Kageyama <takumik.1715@gmail.com>
2026-09-03 13:45:13 +0900

    ネットワークの演習

---
+++ app/src/main/java/jp/ac/meijou/android/s251205044/MainActivity4.java
@@ -0,0 +1,68 @@
+package jp.ac.meijou.android.s251205044;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s251205044.databinding.ActivityMain4Binding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
+    private ActivityMain4Binding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        var request = new Request.Builder()
+//                .url("https://api.github.com/gists/c2a7c39532239ff261be")
+                .url("https://mura.github.io/meijou-android-sample/gist.json")
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // レスポンスボディをGist型に変換
+                var gist = gistJsonAdapter.fromJson(response.body().source());
+                // 中身の取り出し
+                Optional.ofNullable(gist)
+                        .map(g -> g.files.get("OkHttp.txt"))
+                        .ifPresent(gistFile -> {
+                            // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+                            runOnUiThread(() -> binding.text.setText(gistFile.content));
+                        });
+            }
+        });
+    }
+}
